ba94c66700d71732616a65e8481fa48e5b393318
Nikolo <hectorvel52@gmail.com>
2023-04-29 21:31:02 -0500

    Learning about strings

---
+++ IntroduccionJS/js/04.js
@@ -0,0 +1,9 @@
+// Strings
+
+const product = '20" monitor'; // Initialize a string, most used one
+const product2 = String('30" monitor'); // Indicates that what is in the function is a string
+const product3 = new String('50" monitor'); // Initializes as an object String
+
+console.log(product);
+console.log(product2);
+console.log(product3);
