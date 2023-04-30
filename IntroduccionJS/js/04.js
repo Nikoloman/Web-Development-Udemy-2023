2aebbc33c2da6584a5aaad43fd4cb7154a05025e
Nikolo <hectorvel52@gmail.com>
2023-04-29 21:50:02 -0500

    Some of the methods that strings have

--- IntroduccionJS/js/04.js
+++ IntroduccionJS/js/04.js
@@ -7,3 +7,11 @@ const product3 = new String('50" monitor'); // Initializes as an object String
 console.log(product);
 console.log(product2);
 console.log(product3);
+
+// String functions
+
+const product4 = ('These are some of the item in our store');
+
+console.log(product4.length); // Gets the length of the string
+console.log(product4.indexOf('e')); // Gets the first index of a given character, if not found returns -1
+console.log(product4.includes("are")); // Returns if includes a given string or character, returns a boolean value
