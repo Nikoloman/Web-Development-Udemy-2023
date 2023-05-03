6ecbed24d6bbd4f114882076432631b9d42f3156
Nikolo <hectorvel52@gmail.com>
2023-05-02 22:20:59 -0600

    Numbers in JS

---
+++ IntroduccionJS/js/05.js
@@ -0,0 +1,14 @@
+// Numbers
+// In Js there's no such things as ints or float number, in Js there are just numbers
+
+const number1 = 100;
+const number2 = 200;
+const number3 = 10.5;
+const number4 = 0.5;
+const number5 = -2;
+
+console.log(number1 + number2);
+console.log(number1 - number3);
+console.log(number1 * number4);
+console.log(number1 / number5);
+console.log(number1 % number2);
