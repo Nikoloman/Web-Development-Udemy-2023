eab15d2844bc08ec6026be06f0f921dc0b66370a
Nikolo <hectorvel52@gmail.com>
2023-05-03 09:37:11 -0600

    Learning about Math in Js

---
+++ IntroduccionJS/js/06.js
@@ -0,0 +1,33 @@
+// Math Object
+
+var result;
+
+result = Math.PI; // Gets PI
+console.log(result);
+
+result = Math.round(2.2); // Rounds a number
+console.log(result);
+
+result = Math.ceil(3.2); // Always rounds up
+console.log(result);
+
+result = Math.floor(3.8); // Always rounds down a number
+console.log(result);
+
+result = Math.sqrt(25); // Square root
+console.log(result);
+
+result = Math.abs(-20); // Absolute value
+console.log(result);
+
+result = Math.min(2, 9, 5, 6, 1, -2); // Returns the smallest element from a given set of numbers
+console.log(result);
+
+result = Math.max(2, 9, 5, 6, 1, -2); // Returns the biggest element from a given set of numbers
+console.log(result);
+
+result = Math.random(); // Returns a random number from 0 to 1
+console.log(result);
+
+result = Math.floor(Math.random() * 30); // Returns a random number from 0 to 30
+console.log(result);
