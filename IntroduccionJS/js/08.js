82f3ab7ac4e570f58fa864873548379dd914a1a0
Nikolo <hectorvel52@gmail.com>
2023-06-07 08:05:32 -0600

    Concatenation and Template Strings

---
+++ IntroduccionJS/js/08.js
@@ -0,0 +1,10 @@
+// Concatenation and Template Strings
+
+const name = "Hector";
+const email = "sample@domain.com";
+
+// Concatenation
+console.log("Name: " + name + "\nEmail: " + email);
+
+//Template Strings
+console.log(`Client name: ${name} \nClient email: ${email}`);
